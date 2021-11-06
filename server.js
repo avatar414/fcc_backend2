b4c46541d8375f955ae31b45f7c5389ef88a9bc9
avatar414 <david_dexter@hotmail.com>
2021-11-05 19:45:54 -0500

    First Test Version

--- server.js
+++ server.js
@@ -27,12 +27,13 @@ app.get("/api/hello", function (req, res) {
 
 app.get("/api/whoami", (req,res) => {
 
-  // res.send({
-  //   "ipaddress" : req.headers['Remote_Addr'],
-  //   "language" : req.headers['accept-language'],
-  //   "software" : req.headers['user-agent']
-  // });
-  res.send(req.headers);
+  res.send({
+    "ipaddress" : req.headers['x-forwarded-for'],
+    "language" : req.headers['accept-language'].split(",")[0],
+    "software" : req.headers['user-agent']
+  });
+
+  //res.send(req.headers);
 
 });
 
